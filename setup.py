00ea50f8eb8423265a4cc0254a54ca96c7ad3b04
nikhil malagar <nikhilmalagar@gmail.com>
2026-04-24 14:42:51 +0530

    Fixed setup.py for deployment

--- setup.py
+++ setup.py
@@ -1,29 +1,18 @@
 from setuptools import setup, find_packages
-from typing import List
-
-
-
-
-def get_requirements(file_path:str)->List[str]:
-    '''
-    this function will return the list of requirements
-    '''
-    requirements = []
-    with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace("\n","") for req in requirements]
-
-        if '-e .' in requirements:
-            requirements.remove('-e .')
-            
-    return requirements
-
 
 setup(
-    name = "mlproject",
-    version = "0.0.1",
-    author = "Nikhil Malgar",
-    author_email = "nikhilmalagar@gmail.com",
-    packages = find_packages(),
-    install_requires = get_requirements('requirements.txt')  
+    name="mlproject",
+    version="0.0.1",
+    author="Nikhil Malagar",
+    author_email="nikhilmalagar@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "Flask==3.0.3",
+        "numpy==1.24.4",
+        "pandas==2.0.3",
+        "scikit-learn==1.3.2",
+        "scipy==1.10.1",
+        "catboost==1.2.10",
+        "joblib==1.4.2"
+    ],
 )
